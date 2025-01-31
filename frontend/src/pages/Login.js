f57e4d532c9c7b5fc4bcd6c6ed783ec3b39b0193
berkay <berkayherek@gmail.com>
2025-02-01 02:35:04 +0300

    m

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,8 +4,11 @@ import Navbar from "../components/Navbar";
 const LoginPage = () => {
     return (
         <div>
+           <Navbar />
+           <div>
             <h1>Login</h1>
             <LoginForm />
+        </div>
         </div>
     );
 };
